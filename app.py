3ced33641f93d2f14205776ece7d1ac0d7a3f3eb
mdrayhan03 <phylosopherhossain2022@gmail.com>
2025-11-28 19:59:54 +0600

    pwa test 1

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# @app.route('/service_worker.js')
+# def service_worker():
+#     return send_from_directory('.', 'service_worker.js')
+
+if __name__ == "__main__":
+    app.run(debug=True)
